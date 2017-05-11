42931eeb8e7fe286737646686c8ccef238ad06e0
Alessandro Desantis <desa.alessandro@gmail.com>
2017-05-11 16:27:53 +0200

    Fix coding style issues

--- app/concepts/mercury/transport/apn.rb
+++ app/concepts/mercury/transport/apn.rb
@@ -8,7 +8,7 @@ module Mercury
       def deliver!(params:, **)
         params[:notification].recipient.devices.where(type: 'apple').each do |device|
           Rpush::Apns::Notification.create!(
-            app: Rpush::Apns::App.find_by_name('ios_app'),
+            app: Rpush::Apns::App.find_by(name: 'ios_app'),
             device_token: device.source['udid'],
             alert: params[:notification].text,
             data: params[:notification].meta
--- spec/concepts/mercury/notification/deliver_spec.rb
+++ spec/concepts/mercury/notification/deliver_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe Mercury::Notification::Deliver do
   subject(:result) do
     described_class.call({
