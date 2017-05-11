1ed60a66573db14d30d76d7c210d943f868f7c61
Alessandro Desantis <desa.alessandro@gmail.com>
2017-05-11 15:53:22 +0200

    Implement APN transport

--- app/concepts/mercury/transport/apn.rb
+++ app/concepts/mercury/transport/apn.rb
@@ -5,8 +5,15 @@ module Mercury
     class APN < Base
       step :deliver!
 
-      def deliver!(_options)
-        true
+      def deliver!(params:, **)
+        params[:notification].recipient.devices.where(type: 'apple').each do |device|
+          Rpush::Apns::Notification.create!(
+            app: Rpush::Apns::App.find_by_name('ios_app'),
+            device_token: device.source['udid'],
+            alert: params[:notification].text,
+            data: params[:notification].meta
+          )
+        end
       end
     end
   end
