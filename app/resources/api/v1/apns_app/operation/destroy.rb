c7d4412419fe4008a9f4cced990902ca6eacf6fb
Alessandro Desantis <desa.alessandro@gmail.com>
2017-05-16 16:45:08 +0200

    Implement APNS app API

---
+++ app/resources/api/v1/apns_app/operation/destroy.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module API
+  module V1
+    module ApnsApp
+      module Operation
+        class Destroy < Pragma::Operation::Destroy
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :apns_apps, only: %i(index show create update destroy), path: 'apns-apps'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
       resources :pings, only: %i(create)
       resources :profile_groups, only: %i(index show create update destroy), path: 'profile-groups'
       resources :profiles, only: %i(index show create update destroy)
