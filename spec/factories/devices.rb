dd77bb5c728be47ffeca3b9bc4d9dce8f3e0aac7
Alessandro Desantis <desa.alessandro@gmail.com>
2017-05-16 18:32:12 +0200

    Validate APNS device token format

--- spec/factories/devices.rb
+++ spec/factories/devices.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
     type { Device.type.values.sample }
     source do
       {
-        'token' => SecureRandom.uuid,
+        'token' => '740f4707 bebcf74f 9b7c25d4 8e335894 5f6aa01d a5ddb387 462c7eaf 61bb78ad',
         'apns_app' => FactoryGirl.create(:apns_app).id
       }
     end
