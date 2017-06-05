402e00693f76c7b51c7d04f78e0da601b4dcbbca
Alessandro Desantis <desa.alessandro@gmail.com>
2017-06-05 18:18:36 +0200

    fix(apns-apps): Fix STI for APNS apps

--- app/models/apns_app.rb
+++ app/models/apns_app.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ApnsApp < Rpush::Apns::App
+  self.inheritance_column = false
+
+  default_scope -> { where type: 'Rpush::Client::ActiveRecord::Apns::App' }
+
   def devices
     ::Device.where(type: 'apple').where("CAST(source->>'apns_app' AS integer) = ?", id)
   end
