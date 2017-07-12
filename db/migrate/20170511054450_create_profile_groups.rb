30b63d2cbfd6a6bd55d52e051b831cccb39350b9
Alessandro Desantis <desa.alessandro@gmail.com>
2017-07-12 21:54:51 +0200

    Fix UUID extension in migrations

--- db/migrate/20170511054450_create_profile_groups.rb
+++ db/migrate/20170511054450_create_profile_groups.rb
@@ -1,5 +1,7 @@
 class CreateProfileGroups < ActiveRecord::Migration[5.1]
   def change
+    enable_extension 'pgcrypto'
+
     create_table :profile_groups, id: :uuid do |t|
       t.string :name, null: false
     end
