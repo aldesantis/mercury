f6bcbc28583cf3eaf1c67f0e1fccca0e91ea2213
Alessandro Desantis <desa.alessandro@gmail.com>
2017-09-08 13:06:10 +0300

    Implement Pubnub message dispatch

---
+++ app/concepts/mercury/transport/pubnub/client.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Mercury
+  module Transport
+    module Pubnub
+      class Client < SimpleDelegator
+        include Singleton
+
+        class << self
+          def method_missing(method, *args, &block)
+            if instance.respond_to?(method)
+              instance.send(method, *args, &block)
+            else
+              super
+            end
+          end
+
+          def respond_to_missing?(method, include_private = false)
+            instance.respond_to?(method, include_private) || super
+          end
+        end
+
+        def initialize
+          @client = ::Pubnub.new(
+            subscribe_key: ENV.fetch('PUBNUB_SUBSCRIBE_KEY'),
+            publish_key: ENV.fetch('PUBNUB_PUBLISH_KEY'),
+            ssl: true
+          )
+
+          __setobj__ @client
+        end
+      end
+    end
+  end
+end
---
+++ app/concepts/mercury/transport/pubnub/dispatch.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Mercury
+  module Transport
+    module Pubnub
+      class Dispatch < Base
+        step :deliver!
+
+        def deliver!(params:, **)
+          Client.publish(
+            channel: channel_for(params[:notification]),
+            message: {
+              text: params[:notification].text,
+              meta: params[:notification].meta
+            }
+          ) do |envelope|
+            Rails.logger.debug envelope.status
+          end
+        end
+
+        private
+
+        def channel_for(notification)
+          case notification.recipient
+          when ::Profile
+            "profiles:#{notification.recipient.id}"
+          when ::ProfileGroup
+            "profile_groups:#{notification.recipient.id}"
+          end
+        end
+      end
+    end
+  end
+end
