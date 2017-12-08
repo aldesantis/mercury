ad93ec0af811090752ed3618d688fa8e2ff95b92
Alessandro Desantis <desa.alessandro@gmail.com>
2017-12-08 15:15:21 +0100

    Configure deployment for the release branch

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git remote add heroku-dev git@heroku.com:b911-mercury-dev.git
+git remote add heroku-release git@heroku.com:b911-mercury-release.git
 git remote add heroku-staging git@heroku.com:b911-mercury-staging.git
 git remote add heroku-production git@heroku.com:b911-mercury-production.git
 
